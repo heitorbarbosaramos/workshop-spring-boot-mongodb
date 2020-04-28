1aee3572a1bca41a3c260f6a181c8475a697e354
Heitor Ramos <heitorhfbr@gmail.com>
2020-04-28 11:34:16 -0300

    Projeto Criado

---
+++ src/test/java/com/heitor/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heitor.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
